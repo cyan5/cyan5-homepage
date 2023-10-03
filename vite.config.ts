fd62f7fe0c2fa49ff446295380f96dec5b47065a
cyan5 <r.vanis2000@gmail.com>
2023-10-04 02:05:56 +0900

    パッケージの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import react from '@vitejs/plugin-react'
+import linaria from '@linaria/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default defineConfig(() => ({
+  // ...
+  plugins: [linaria()],
+}));
