e5b7bcffb447084dc311fca87dac5cbd23dfd0ba
cyan5 <r.vanis2000@gmail.com>
2023-10-05 01:54:13 +0900

    Aboutページに要素を追加

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 // About.tsx
 
+import { styled } from "@linaria/react";
 import Cover from "../components/Cover";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
@@ -11,11 +12,61 @@ const About = () => {
       <NavBar />
       <Cover />
       <StyledContents>
-        <h1>This is About Page.</h1>
+        <h1>About</h1>
+        <StyledImage>image</StyledImage>
+        <StyledH2>シアン / cyan</StyledH2>
+        <h2>Outline</h2>
+        <p>
+          バックエンドに興味があり、現在はGoの学習をしています。就職までにRust、MySQLを学習予定です。
+        </p>
+        <h2>Skills</h2>
+          <ul>
+            <li>C/C++</li>
+            <li>Python</li>
+            <li>TypeScript + React</li>
+            <li>(Go)</li>
+            <li>(Rust)</li>
+            <li>(MySQL)</li>
+          </ul>
+        <h2>Career</h2>
+          <ul>
+            <li>2019年4月 東京工業大学 情報理工学院 入学</li>
+            <li>2023年3月 東京工業大学 情報理工学院 情報工学系 卒業</li>
+            <li>2023年4月 東京工業大学大学院 情報工学系 知能情報コース 入学</li>
+          </ul>
+        <h2>Affiliation</h2>
+          <ul>
+            <li><a href='http://www.sc.dis.titech.ac.jp/' target="_blank">石井研究室</a> (2022.4 ～)</li>
+            <li><a href='https://www.chorkleines.com/' target="_blank">東京工業大学混声合唱団コール・クライネス</a> (2019.4 ～ 2023.3)</li>
+          </ul>
+        <h2>Links</h2>
+          <ul>
+            <li><a href='https://twitter.com/r_vanis' target="_blank">X (旧Twitter)</a></li>
+            <li><a href='https://github.com/cyan5' target="_blank">GitHub</a></li>
+          </ul>
       </StyledContents>
       <Footer />
     </div>
   );
 };
+
+const StyledImage = styled.div`
+  text-align: center;
+  color: transparent;
+  background: cyan;
+  background-image: url(src/images/cyan.png);
+  background-size: 100%;
+  width: 200px;
+  height: 200px;
+  border: 1px;
+  margin: auto;
+  border-style: solid;
+  border-color: black;
+  border-radius: 50%;
+`
+
+const StyledH2 = styled.h2`
+  text-align: center;
+`
 
 export default About;
--- src/styles/StyledContents.ts
+++ src/styles/StyledContents.ts
@@ -1,10 +1,12 @@
 import { styled } from "@linaria/react"
 
 export const StyledContents = styled.div`
-  /* width: auto; */
   max-width: 728px;
+  min-height: 40vh;
   padding: 1em;
   margin: auto;
-  /* margin-top: 50px; */
-  min-height: 40vh;
+
+  p {
+    text-indent: 1rem;
+  }
 `
