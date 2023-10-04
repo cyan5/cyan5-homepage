e31910a289976f2e4a794047b9cdff9c89e4bfa8
cyan5 <r.vanis2000@gmail.com>
2023-10-04 16:10:50 +0900

    スタイルの切り離し

---
+++ src/styles/StyledContents.ts
@@ -0,0 +1,10 @@
+import { styled } from "@linaria/react"
+
+export const StyledContents = styled.div`
+  /* width: auto; */
+  max-width: 728px;
+  padding: 1em;
+  margin: auto;
+  /* margin-top: 50px; */
+  min-height: 40vh;
+`
