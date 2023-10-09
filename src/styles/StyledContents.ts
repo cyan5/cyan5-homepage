fe7c1c27d9a4d3e726179f1820a4e39f07ba908c
cyan5 <r.vanis2000@gmail.com>
2023-10-10 02:14:43 +0900

    h1スタイルの変更

--- src/styles/StyledContents.ts
+++ src/styles/StyledContents.ts
@@ -5,9 +5,28 @@ export const StyledContents = styled.div`
   min-height: 40vh;
   padding: 1em;
   margin: auto;
+  /* background-color: #e7efff; */
 
   h1 {
-    margin: 6vh 0 0 0;
+    width: fit-content;
+    padding: 0 10px;
+    margin: 6vh 0 3vh 0;
+    border-style: solid;
+    border-top-width: 0;
+    border-left-width: 0;
+    border-right-width: 0;
+    border-bottom-width: 2px;
+  }
+
+  h2 {
+    background-color: #eeefff;
+    padding: 0px 15px;
+    border-style: solid;
+    border-color: #174f7f;
+    border-top-width: 0px;
+    border-left-width: 5px;
+    border-right-width: 0px;
+    border-bottom-width: 0px;
   }
 
   p {
