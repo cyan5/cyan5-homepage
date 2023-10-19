fba657013338b8b7f9d8458f8878ddb0d430ce38
cyan5 <r.vanis2000@gmail.com>
2023-10-20 00:41:15 +0900

    WorkItemからStyleを切り離し

---
+++ src/styles/StyledWorkItem.ts
@@ -0,0 +1,69 @@
+import { styled } from "@linaria/react";
+
+export const StyledWorkItem = styled.li`
+  list-style: none;
+  width: calc(97% / 3);
+  /* width: 248px; */
+  margin: 0.5%;
+  height: 220px;
+  border: 1.2px;
+  border-style: solid;
+  border-color: gray;
+  border-radius: 10px;
+  position: relative;
+  cursor: pointer;
+
+  @media only screen and (max-width: 790px) {
+    width: calc(98% / 2);
+  }
+
+  @media only screen and (max-width: 540px) {
+    width: calc(99%);
+  }
+
+  :hover {
+    opacity: 0.8;
+  }
+
+  img {
+    object-fit: cover;
+    display: block;
+    width: 100%;
+    height: 67%;
+    border-radius: 8px 8px 0px 0px;
+    margin: 0;
+  }
+
+  div {
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    border-radius: 0px 0px 8px 8px;
+    padding: 6px 20px;
+    margin: 0;
+    box-sizing: border-box;
+
+    h4 {
+      margin: 0;
+    }
+
+    ul {
+      padding: 0;
+      margin: 0;
+
+      li {
+        list-style: none;
+        display: inline-flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        font-size: 8pt;
+        font-weight: bold;
+        color: #555555;
+        background: #dddddd;
+        margin: 0px 4px 0px 0px;
+        padding: 2px 4px;
+        border-radius: 6px;
+      }
+    }
+  }
+`
