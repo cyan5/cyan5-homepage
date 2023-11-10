9e0d6cdbc1c300ddb2af337ca5cbb9d4b5a6b71a
cyan5 <r.vanis2000@gmail.com>
2023-11-11 07:09:08 +0900

    画像が表示されるようにする

--- src/components/Works/WorksData.ts
+++ src/components/Works/WorksData.ts
@@ -1,7 +1,10 @@
+import srcFuji from "../../images/DSC_0050.jpg"
+
 interface worksDataIF {
   cyansHP: {
     title: string;
     date: string;
+    srcImage: string
     tags: string[];
     outline: string;
     links: {
@@ -13,6 +16,7 @@ interface worksDataIF {
   [worksDataKey: string]: {
     title: string;
     date: string;
+    srcImage: string
     tags: string[];
     outline: string;
     links: {
@@ -27,6 +31,7 @@ export const worksData: worksDataIF = {
   cyansHP: {
     title: "cyan's Homepage", 
     date: "2023.11.11", 
+    srcImage: srcFuji, 
     tags: ["TypeScript + React", "WebApp"], 
     outline: "当ウェブサイト。プロダクトや趣味をわかりやすくするために作成。今後も作品が追加され次第更新予定。", 
     links: [
