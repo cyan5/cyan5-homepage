5cdff4e3483e0a0682cfc41c1fcf930751c4cb93
cyan5 <r.vanis2000@gmail.com>
2023-11-14 02:55:31 +0900

    カバーのスタイル変更

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -1,31 +1,31 @@
 // Cover.tsx
 
 import { styled } from '@linaria/react'
-import CoverImg from '../images/DSC_0050.jpg'
-
+import srcFuji from '../images/DSC_0050.jpg'
 
 const Cover = () => {
   return(
     <StyledCover>
-      <img src={CoverImg}></img>
+      {/* <img src={CoverImg}></img> */}
     </StyledCover>
   );
 };
 
 const StyledCover = styled.div`
+  background-image: url(${srcFuji});
   background-size: cover;
   background-position: center;
-  height: 50vh;
+  height: 25vh;
   padding: 0; 
   margin: 10 20 30 40;
-  background: #007070;
+  /* background: #007070; */
 
-  img {
-    /* size: cover; */
+  /* img {
+    size: cover;
     width: 100%;
     height: 100%;
     object-fit: cover;
-  }
+  } */
 `
 
 export default Cover;
