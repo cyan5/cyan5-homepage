b6e9701a03b16e36fca9e96b67eecd4d780a4396
cyan5 <r.vanis2000@gmail.com>
2023-11-14 00:41:22 +0900

    ページpaddingの修正

--- src/styles/StyledContents.ts
+++ src/styles/StyledContents.ts
@@ -2,13 +2,14 @@ import { styled } from "@linaria/react"
 
 export const StyledContents = styled.div`
   max-width: 1000px;
-  padding: 6vh 10px;
+  padding: 6vh calc(15px + 1vw);
   padding: auto;
   margin: auto;
 
   p {
     font-family: 'Raleway';
     padding-left: 45px;
+    margin: 0;
     text-indent: 1rem;
   }
 
