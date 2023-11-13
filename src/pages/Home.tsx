15a52d1852b3b5d9fd52a30cc8c3a24b4d8bc067
cyan5 <r.vanis2000@gmail.com>
2023-11-13 16:28:54 +0900

    Homeデザインの変更

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,16 @@
 // import './Home.css'
 import { styled } from '@linaria/react'
-import Cover from '../components/Cover'
 import NavBar from '../components/NavBar'
-import BigIcon from '../components/BigIcon'
-import Footer from '../components/Footer'
 import { Link } from 'react-router-dom'
 import { StyledContents } from '../styles/StyledContents'
+import srcFuji from "../images/DSC_0050.jpg"
 
 const Home = () => {
   return (
     <StyledHome>
       <NavBar />
-      <BigIcon />
-      <Cover />
+      {/* <BigIcon /> */}
+      {/* <Cover /> */}
       <StyledContents>
         <h1>cyan's Homepage</h1>
         <ul>
@@ -20,13 +18,18 @@ const Home = () => {
           <li><h2><Link to='Works'>Works</Link></h2></li>
         </ul>
       </StyledContents>
-      <Footer />
+      {/* <Footer /> */}
     </StyledHome>
   )
 }
 
 const StyledHome = styled.div`
   background: white;
+  background-size: cover;
+  background-position: bottom center;
+  background-image: url(${srcFuji});
+  width: 100%;
+  height: 100vh;
   color: #1f1f1f;
 `
 
