6ea452e03cec06b16bd980314bc1ab1660022fd7
cyan5 <r.vanis2000@gmail.com>
2023-11-14 20:23:40 +0900

    paddingの変更

--- src/styles/StyledContents.ts
+++ src/styles/StyledContents.ts
@@ -2,11 +2,11 @@ import { styled } from "@linaria/react"
 
 export const StyledContents = styled.div`
   max-width: 1000px;
-  padding: calc(30px + 1vh) calc(15px + 1vw);
+  padding: calc(30px + 1vh) 10px;
   margin: auto;
 
   p {
-    padding-left: 54px;
+    padding-left: 30px;
     margin: 0;
     text-indent: 1rem;
   }
--- src/styles/StyledSection.ts
+++ src/styles/StyledSection.ts
@@ -2,13 +2,13 @@ import { styled } from "@linaria/react"
 
 export const StyledSection = styled.div`
   ul {
-    padding: 0 40px;
+    padding: 0 25px;
     width: fit-content;
     li {
       width: fit-content;
       h2 {
         width: fit-content;
-        padding: 0px 15px;
+        padding: 0px 5px;
         text-decoration: underline;
         filter: drop-shadow(1px 1px 1.3px #bbbbbb);
       }
